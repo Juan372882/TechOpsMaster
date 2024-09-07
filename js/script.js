215e9ed892570e58eb72dda950933b071201cbe8
Juan372882 <jmmartinez4817@misena.edu.co>
2024-09-07 12:25:30 -0500

    Live Switch color, animated typing and, dark/light mode :)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Web Designer","Mobile Developer","Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
