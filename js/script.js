6ee6c78fca08b18de2d1765243ff322c7e5e6994
Juan <jmmartinez4817@misena.edu.co>
2024-09-09 13:21:41 -0500

    Testing form

--- js/script.js
+++ js/script.js
@@ -90,3 +90,13 @@ const nav = document.querySelector(".nav"),
             allSection[i].classList.toggle("open");
         }
     }
+    function submitForm() {
+        // Get the first form with the name
+        // Usually the form name is not repeated
+        // but duplicate names are possible in HTML
+        // Therefore to work around the issue, enforce the correct index
+        var frm = document.getElementsByName('contact-form')[0];
+        frm.submit(); // Submit the form
+        frm.reset();  // Reset all form data
+        return false; // Prevent page refresh
+     }
